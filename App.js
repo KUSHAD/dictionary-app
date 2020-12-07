704f5f70a5e33af6cdcdf42bac2c2df63d5d68be
KUSHAD <kushadck@gmail.com>
2020-12-07 10:30:16 +0530

    made a custom disbaled button style

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
+import React, { Component } from 'react';
 import {
 	StyleSheet,
 	Text,
@@ -8,23 +8,56 @@ import {
 	View,
 } from 'react-native';
 
-const App = () => {
-	return (
-		<>
-			<View style={styles.header}>
-				<Ionicons name="book" size={24} color="white" />
-				<Text style={styles.headerText}> Pocket Dictionary</Text>
-			</View>
-			<View style={styles.container}>
-				<TextInput style={styles.input} placeholder="Search Words ...." />
-				<TouchableOpacity style={styles.button}>
-					<Ionicons name="search" size={24} color="white" />
-					<Text style={styles.buttonText}> Search</Text>
-				</TouchableOpacity>
-			</View>
-		</>
-	);
-};
+class App extends Component {
+	state = {
+		wordToSearch: '',
+		isbuttonDisabled: true,
+	};
+	searchDictionary = (e) => {
+		const { wordToSearch, isbuttonDisabled } = this.state;
+		if (!wordToSearch) {
+			e.preventDefault();
+		} else {
+			console.log(wordToSearch);
+			this.setState({
+				wordToSearch: '',
+				isbuttonDisabled: true,
+			});
+		}
+	};
+	render() {
+		const { wordToSearch, isbuttonDisabled } = this.state;
+		return (
+			<>
+				<View style={styles.header}>
+					<Ionicons name="book" size={24} color="white" />
+					<Text style={styles.headerText}> Pocket Dictionary</Text>
+				</View>
+				<View style={styles.container}>
+					<TextInput
+						value={wordToSearch}
+						onChange={(e) => {
+							this.setState({
+								wordToSearch: e.target.value,
+								isbuttonDisabled: false,
+							});
+						}}
+						style={styles.input}
+						placeholder="Search Words ...."
+					/>
+					<TouchableOpacity
+						disabled={isbuttonDisabled}
+						onPress={this.searchDictionary}
+						style={isbuttonDisabled ? styles.disabledButton : styles.button}
+					>
+						<Ionicons name="search" size={24} color="white" />
+						<Text style={styles.buttonText}> Search</Text>
+					</TouchableOpacity>
+				</View>
+			</>
+		);
+	}
+}
 
 export default App;
 
@@ -73,5 +106,19 @@ const styles = StyleSheet.create({
 		color: '#fff',
 		fontWeight: 'bold',
 		fontSize: '3vh',
+	},
+	disabledButton: {
+		display: 'flex',
+		flexDirection: 'row',
+		backgroundColor: '#5b737c',
+		height: '6vh',
+		width: '25%',
+		borderRadius: 100,
+		borderWidth: '2px',
+		borderColor: '#000000',
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: '6vh',
+		opacity: 0.3,
 	},
 });
